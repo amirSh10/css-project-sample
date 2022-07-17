2469122157b0f110fb6588dd14ea7499838b73cc
unknown <shahidia851@gmail.com>
2022-07-17 18:15:41 +0430

    add some style and component

---
+++ src/component/Form.js
@@ -0,0 +1,20 @@
+import "./style/style.css";
+function Form_date() {
+  return (
+    <>
+      <main className="flex_of_main_tag">
+        <body className="flex_of_body_tag">
+          <section className="flex_of_section_tag">
+            <article className="styel_of_header_article">
+              aplication for permission to date my daughter
+            </article>
+            <article className="style_of_text_article">
+              Not:Form is to be completed at least 21 days prior to date
+            </article>
+          </section>
+        </body>
+      </main>
+    </>
+  );
+}
+export default Form_date;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+import  Form_date  from './component/Form.js';
+
+function App(){
+   return(<> 
+   <Form_date/>
+   </>)
+}
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
