7477b4e006fd122031af8b332316dc3f678bc07f
unknown <shahidia851@gmail.com>
2022-07-18 14:10:53 +0430

    add form

--- src/component/Form.js
+++ src/component/Form.js
@@ -2,15 +2,27 @@ import "./style/style.css";
 function Form_date() {
   return (
     <>
-      <main className="flex_of_main_tag">
+      <main className="flex_of_main_tag" >
         <body className="flex_of_body_tag">
           <section className="flex_of_section_tag">
-            <article className="styel_of_header_article">
+            <div className="aa">
+            <p className="styel_of_header_article">
               aplication for permission to date my daughter
-            </article>
-            <article className="style_of_text_article">
-              Not:Form is to be completed at least 21 days prior to date
-            </article>
+            </p>
+            </div>
+            <p className="style_of_text_article">
+              <b>Not:</b>Form is to be completed at least 21 days prior to date
+            </p>
+
+            <fieldset className="margin_of_filedset">
+            <legend style={{fontWeight:"900"}}>Personal Details</legend>
+            <div>
+              <form>
+                <label>Name:</label>
+                <input type={Text}></input>
+              </form>
+            </div>
+            </fieldset>
           </section>
         </body>
       </main>
