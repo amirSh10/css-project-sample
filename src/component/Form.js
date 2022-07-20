9163c0e0425c93f96c27ad84035420ee001568eb
unknown <shahidia851@gmail.com>
2022-07-20 20:20:04 +0430

    complete client side

--- src/component/Form.js
+++ src/component/Form.js
@@ -93,6 +93,107 @@ function Form_date() {
                     </div>
                   </div>
                 </fieldset>
+                <fieldset className="margin_of_filedset">
+                  <p>
+                    <b>Check All That Apply</b>
+                  </p>
+                  <div className="flex_of_form_cloume">
+                    <div>
+                      <label className="style_of_label_form">
+                        <input type={"checkbox"}></input> I have tattoos and/or
+                        piercings
+                      </label>
+                    </div>
+                    <div>
+                      <label className="style_of_label_form">
+                        <input type={"checkbox"}></input> I am more than 2 years
+                        older than my daughter
+                      </label>
+                    </div>
+                    <div>
+                      <label className="style_of_label_form">
+                        <input type={"checkbox"}></input> I own a panel van or
+                        V8 ute
+                      </label>
+                    </div>
+                    <div>
+                      <label className="style_of_label_form">
+                        <input type={"checkbox"}></input> I work full-time
+                      </label>
+                    </div>
+                    <div>
+                      <label className="style_of_label_form">
+                        <input type={"checkbox"}></input> My parents are rich
+                      </label>
+                    </div>
+                    <div>
+                      <label className="style_of_label_form">
+                        <input type={"checkbox"}></input> Is the date at a well
+                        lit public location
+                      </label>
+                    </div>
+                  </div>
+                </fieldset>
+                <fieldset className="margin_of_filedset">
+                  <div className="flex_of_select_option_row">
+                    <div>
+                      <label>
+                        Political Persuasion:{" "}
+                        <select>
+                          <option>Left Wing</option>
+                          <option>Right Wing</option>
+                          <option>Conservative</option>
+                          <option>Nazi</option>
+                        </select>
+                      </label>
+                    </div>
+                    <div>
+                      <label>
+                        Education Level Completed::{" "}
+                        <select>
+                          <option>University</option>
+                          <option>College</option>
+                          <option>High School </option>
+                          <option>None</option>
+                        </select>
+                      </label>
+                    </div>
+                  </div>
+                </fieldset>
+                {/* easy section */}
+                <fieldset className="margin_of_filedset">
+                  <legend style={{ fontWeight: "900" }}>Easy Section</legend>
+                  <p className="">
+                    <b>
+                      In 50 words or more explain why you want to date my
+                      daughter
+                    </b>
+                  </p>
+                  <textarea
+                    placeholder="Enter Text Here"
+                    cols={50}
+                    rows={5}
+                    style={{ resize: "none" }}
+                  ></textarea>
+                  <p className="">
+                    <b>Please upload contact details for 2 references</b>
+                  </p>
+                  <textarea
+                    placeholder="Enter Text Here"
+                    cols={50}
+                    rows={5}
+                    style={{ resize: "none" }}
+                  ></textarea>
+                  <p className="">
+                    <b>
+                    Upload Police Clearance Certificate, Bank Statement and Medical Certifiates here:
+                    </b>
+                    <button className="style_of_btn_servery_form">Attach Files</button>
+                  </p>
+                </fieldset>
+                <div className="flex_of_btn_to_set_center">
+                <button className="style_of_btn_servery_form second_style_of_btn_send"> Send Your Application</button>
+                </div>
               </form>
             </div>
           </section>
